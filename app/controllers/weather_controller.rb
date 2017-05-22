e6806c83fad803d97ca00bf3c797aa4e02e157fd
Yatish <yatiish@gmail.com>
2017-05-22 11:57:49 +0000

    Integrate weather api gem

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,10 +1,12 @@
 class WeatherController < ApplicationController
+  require 'open_weather'
 
   def get_coordinates
   end
   
   def get_weather
-    @abc = params["city"]
-	@dfd = params["country"]
+    options = { units: "metric", APPID: Rails.application.secrets.weather_api }
+    @weather_details = OpenWeather::Current.city("#{params["city"]}, #{params["country"]}", options)
+	@des = @weather_details["weather"]
   end
 end
